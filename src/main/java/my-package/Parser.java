78c054fb6389365a15f103b87eab780ea732c4ef
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:32:14 +0300

    Scripted auto-commit on change (2021-12-21 06:32:14) by gitwatch.sh

--- src/main/java/my-package/Parser.java
+++ src/main/java/my-package/Parser.java
@@ -21,7 +21,7 @@ public class Parser {
         return flights.map(str -> str.split(DELIMITER))
                       .map(str -> new Flight(str[IS_CANCELLED].equals(CANCELLED_FLAG),
                                              Float.parseFloat(str[DELAY_TIME]),
-
+                                             Integer.parseInt(str[ORIGIN_AIRPORT_ID]),
 
     }
 
