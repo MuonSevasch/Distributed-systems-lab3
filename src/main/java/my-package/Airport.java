cd3e5735a60ca14e06879b36884b16ac1a638af8
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:35:46 +0300

    Scripted auto-commit on change (2021-12-21 06:35:46) by gitwatch.sh

--- src/main/java/my-package/Airport.java
+++ src/main/java/my-package/Airport.java
@@ -21,7 +21,7 @@ public class Airport implements Serializable {
 
     @Override
     public String toString() {
-        return "Airport{" +
+
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
