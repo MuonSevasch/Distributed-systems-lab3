7415d29666b09f334fea189b5df7ec53fd0278d5
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:11:25 +0300

    Scripted auto-commit on change (2021-12-21 06:11:25) by gitwatch.sh

--- src/main/java/my-package/App.java
+++ src/main/java/my-package/App.java
@@ -14,7 +14,7 @@ public class App {
     private static final String AIRPORTS_DATA_FILENAME="airports.csv";
     private static final String FLIGHTS_DATA_FILENAME="flights.csv";
 
-
+    public App(SparkConf conf){
         this.conf = conf;
     }
 
