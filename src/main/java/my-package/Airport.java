05626ab16e3a61fc2bf623b668d6ee99af1d0cee
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:01:07 +0300

    Scripted auto-commit on change (2021-12-21 06:01:07) by gitwatch.sh

--- src/main/java/my-package/Airport.java
+++ src/main/java/my-package/Airport.java
@@ -3,7 +3,7 @@ package ru.bmstu.lab3;
 import java.io.Serializable;
 
 public class Airport implements Serializable {
-
+    private final int id;
     private final String name;
 
     public Airport(int id, String name) {
