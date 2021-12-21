8f172d073f8f1e4890e1c1b40d8ed35d32ef7545
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:48:53 +0300

    Scripted auto-commit on change (2021-12-21 06:48:53) by gitwatch.sh

--- src/main/java/my-package/Flight.java
+++ src/main/java/my-package/Flight.java
@@ -9,7 +9,7 @@ public class Flight {
     public Flight(boolean isCancelled, float delayTime, int airportId, int destId) {
         this.isCancelled = isCancelled;
         this.delayTime = delayTime;
-
+        this.airportId = airportId;
         this.destId = destId;
     }
 
