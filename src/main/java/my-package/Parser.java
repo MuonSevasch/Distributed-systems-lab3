9ace685a9fc2272e6d01c23a367533cc145dcdef
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:30:54 +0300

    Scripted auto-commit on change (2021-12-21 06:30:54) by gitwatch.sh

--- src/main/java/my-package/Parser.java
+++ src/main/java/my-package/Parser.java
@@ -14,7 +14,7 @@ public class Parser {
     private static final int ORIGIN_AIRPORT_ID = 11;
     private static final int AIRPORT_NAME = 1;
 
-
+    private static final String CANCELLED_FLAG = "1.00";
 
 
     public static JavaRDD<Flight> parseFlightsRDD(JavaRDD<String> flights){
