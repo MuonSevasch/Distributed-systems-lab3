09509d1bd67221ad27ca2820cb504096498ec691
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:05:03 +0300

    Scripted auto-commit on change (2021-12-21 06:05:03) by gitwatch.sh

--- src/main/java/my-package/App.java
+++ src/main/java/my-package/App.java
@@ -11,7 +11,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class App {
 
     private static final  String OUTPUT_FILEPATH = "output";
-
+    private static final String AIRPORTS_DATA_FILENAME="airports.csv";
 
 
 
