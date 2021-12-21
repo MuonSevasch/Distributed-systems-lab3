e73a83846b43c44fe01af6364339abcfd8263935
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:10:07 +0300

    Scripted auto-commit on change (2021-12-21 06:10:07) by gitwatch.sh

--- src/main/java/my-package/App.java
+++ src/main/java/my-package/App.java
@@ -18,7 +18,7 @@ public class App {
         this.conf = conf;
 
 
-
+    public void run(){
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaPairRDD<Integer, Airport> airports = Parser.parseAirports(sc.textFile(AIRPORTS_DATA_FILENAME));
         JavaPairRDD<Tuple2<Integer, Integer>, Flight> flights = Parser.parseFlightsPairRDD(sc.textFile(FLIGHTS_DATA_FILENAME));
