cbd02626d6b201bbea343df471eb8af728901a8e
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:19:15 +0300

    Scripted auto-commit on change (2021-12-21 06:19:15) by gitwatch.sh

--- src/main/java/my-package/Flight.java
+++ src/main/java/my-package/Flight.java
@@ -34,7 +34,7 @@ public class Flight {
         return "Flight{" +
                 "isCancelled=" + isCancelled +
                 ", delayTime=" + delayTime +
-
+                ", airportId=" + airportId +
 
                 '}';
 
