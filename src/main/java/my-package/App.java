d1c57e461f0cd728f92f9fa996678ad5bfae5058
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:37:29 +0300

    Scripted auto-commit on change (2021-12-21 06:37:29) by gitwatch.sh

--- src/main/java/my-package/App.java
+++ src/main/java/my-package/App.java
@@ -1,6 +1,6 @@
 package ru.bmstu.lab3;
 
-import org.apache.spark.SparkConf;
+
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
