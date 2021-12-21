0ef7fb1e13f981e6396905cfbea7f30ffa6b2dc8
MuonSevasch <muon.kvark@yandex.ru>
2021-12-21 06:22:54 +0300

    Scripted auto-commit on change (2021-12-21 06:22:54) by gitwatch.sh

--- src/main/java/my-package/Main.java
+++ src/main/java/my-package/Main.java
@@ -5,7 +5,7 @@ import org.apache.spark.SparkConf;
 public class Main {
     private static final String APP_NAME = "Lab_03";
 
-
+    public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
 
     }
